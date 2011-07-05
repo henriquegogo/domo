b900a141cc1a5fced5211867a98bb346a2232c33
Henrique Soares <henrique@trabalho.(none)>
2011-07-05 20:28:17 -0300

    All fist-level childrens

--- jquery-objective.js
+++ jquery-objective.js
@@ -1,7 +1,17 @@
+jQuery.fn.extend({
+  objective: function() {
+    var result = {};
+    
+    result = jQuery(this).children();
+    
+    return result;
+  }
+});
+
 // jQuery.fn.extend({
 //   objective: function() {
 //     var result = {};
-//     jQuery('> *', this).each(function() {
+//     jQuery('[name]', this).each(function() {
 //       var key = $(this).attr('name');
 //       result[key] = (jQuery('[name]', this).size() > 0 && !jQuery.isArray($(this).val())) ? jQuery(this).objective() :
 //                     jQuery(this).val() ? jQuery(this).val() : jQuery(this).text();
@@ -9,36 +19,3 @@
 //     return result;
 //   }
 // });
-
-
-/*
-HAMOM
-function (elemento) {
-  var filhosComName = []
-    var todosOsFilhos = $('> *', elemento)
-
-    for (var i = 0; i < todosOsFilhos.length; i++) {
-      if ($(todosOsFilhos[i]).attr('name'))
-        filhosComName.push(todosOsFilhos[i])
-      else if ($(todosOsFilhos[i]).children().length > 0) {
-        var netos = selecionaFilhosComName(todosOsFilhos[i])
-
-          filhosComName = filhosComName.concat(netos);
-      }
-    }
-
-  return filhosComName
-} 
-*/
-
-jQuery.fn.extend({
-  objective: function() {
-    var result = {};
-      jQuery('[name]', this).each(function() {
-      var key = $(this).attr('name');
-      result[key] = (jQuery('[name]', this).size() > 0 && !jQuery.isArray($(this).val())) ? jQuery(this).objective() :
-                    jQuery(this).val() ? jQuery(this).val() : jQuery(this).text();
-    });
-    return result;
-  }
-});
