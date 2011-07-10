448b7d1175ebe7e5251a62494760220e39e933f0
Henrique Soares <henrique@vostro.(none)>
2011-07-10 19:04:27 -0300

    Children and sons... great

--- jquery-objective.js
+++ jquery-objective.js
@@ -2,7 +2,17 @@ jQuery.fn.extend({
   objective: function() {
     var result = {};
     
-    result = jQuery(this).children();
+    // result = jQuery(this).children();
+    
+    jQuery(this).children().each(function() {
+      var key = jQuery(this)[0].tagName;
+
+      var value = jQuery(this).children().size() > 0 ? jQuery(this).objective() :
+                  jQuery(this).val() ? $(this).val() : jQuery(this).text();
+
+      result[key] = result[key] ? result[key] = Array(result[key], value) : value;
+
+    });
     
     return result;
   }
