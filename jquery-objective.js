de93a75e99d4b3a8623158f73c95c38335d151aa
Henrique Soares <henrique@vostro.(none)>
2011-07-10 23:13:24 -0300

    Ok, now HTML to JSON its OK

--- jquery-objective.js
+++ jquery-objective.js
@@ -1,8 +1,7 @@
 jQuery.fn.extend({
   objective: function() {
     var result = {};
-    
-    // result = jQuery(this).children();
+    var append = function(array, value) { array.push(value); return array }
     
     jQuery(this).children().each(function() {
       var key = jQuery(this)[0].tagName;
@@ -10,7 +9,8 @@ jQuery.fn.extend({
       var value = jQuery(this).children().size() > 0 ? jQuery(this).objective() :
                   jQuery(this).val() ? $(this).val() : jQuery(this).text();
 
-      result[key] = result[key] ? result[key] = Array(result[key], value) : value;
+      result[key] = (result[key] && !jQuery.isArray(result[key])) ? Array(result[key], value) :
+                    jQuery.isArray(result[key]) ? append(result[key], value) : value;
 
     });
     
