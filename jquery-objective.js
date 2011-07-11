55dcda2233ad3c0d73301060ab679f808af43cf0
Henrique Soares <henrique@vostro.(none)>
2011-07-10 23:15:09 -0300

    Removing commented code and just this

--- jquery-objective.js
+++ jquery-objective.js
@@ -1,7 +1,7 @@
 jQuery.fn.extend({
   objective: function() {
-    var result = {};
     var append = function(array, value) { array.push(value); return array }
+    var result = {};
     
     jQuery(this).children().each(function() {
       var key = jQuery(this)[0].tagName;
@@ -11,21 +11,9 @@ jQuery.fn.extend({
 
       result[key] = (result[key] && !jQuery.isArray(result[key])) ? Array(result[key], value) :
                     jQuery.isArray(result[key]) ? append(result[key], value) : value;
-
     });
     
     return result;
   }
 });
 
-// jQuery.fn.extend({
-//   objective: function() {
-//     var result = {};
-//     jQuery('[name]', this).each(function() {
-//       var key = $(this).attr('name');
-//       result[key] = (jQuery('[name]', this).size() > 0 && !jQuery.isArray($(this).val())) ? jQuery(this).objective() :
-//                     jQuery(this).val() ? jQuery(this).val() : jQuery(this).text();
-//     });
-//     return result;
-//   }
-// });
