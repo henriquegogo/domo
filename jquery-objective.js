1394fecd9c33335c41cae737f14147359f91420d
Henrique Gogó <henriquegogo@gmail.com>
2011-11-03 11:38:46 -0300

    Fixing select and multiselect elements selector

--- jquery-objective.js
+++ jquery-objective.js
@@ -1,20 +1,19 @@
-jQuery.fn.extend({
-  objective: function() {
-    var append = function(array, value) { array.push(value); return array }
-    var result = {};
-    
-    jQuery(this).children().each(function() {
-      var who = jQuery(this).attr('name') ? jQuery(this) : jQuery(this).children('[name]');
-      var key = who.attr('name');
+jQuery.fn.objective = function() {
+  var append = function(array, value) { array.push(value); return array }
+  var result = {};
+  
+  jQuery(this).children().each(function() {
+    var who = jQuery(this).attr('name') ? jQuery(this) : jQuery(this).children('[name]');
+    var key = who.attr('name');
 
-      var value = who.children().size() > 0 ? who.objective() :
-                  who.val() ? who.val() : who.text();
+    var value = who.children('[name]').size() > 0 && !who.is('select') ? who.objective() :
+                who.val() ? who.val() :
+                who.is('select') ? who.val() : who.text();
 
-      if (key) result[key] = (result[key] && !jQuery.isArray(result[key])) ? Array(result[key], value) :
-                             jQuery.isArray(result[key]) ? append(result[key], value) : value;
-    });
-    
-    return result;
-  }
-});
+    if (key) result[key] = (result[key] && !jQuery.isArray(result[key])) ? Array(result[key], value) :
+                           jQuery.isArray(result[key]) ? append(result[key], value) : value;
+  });
+  
+  return result;
+};
 
