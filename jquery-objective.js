81cf471db84ed6ac8f7aa85fbf762009624cce03
Henrique Gogó <henriquegogo@gmail.com>
2011-11-03 11:45:31 -0300

    Closure

--- jquery-objective.js
+++ jquery-objective.js
@@ -1,19 +1,20 @@
-jQuery.fn.objective = function() {
-  var append = function(array, value) { array.push(value); return array }
-  var result = {};
-  
-  jQuery(this).children().each(function() {
-    var who = jQuery(this).attr('name') ? jQuery(this) : jQuery(this).children('[name]');
-    var key = who.attr('name');
+(function ($) {
+  $.fn.objective = function() {
+    var append = function(array, value) { array.push(value); return array }
+    var result = {};
+    
+    $(this).children().each(function() {
+      var who = $(this).attr('name') ? $(this) : $(this).children('[name]');
+      var key = who.attr('name');
 
-    var value = who.children('[name]').size() > 0 && !who.is('select') ? who.objective() :
-                who.val() ? who.val() :
-                who.is('select') ? who.val() : who.text();
+      var value = who.children('[name]').size() > 0 && !who.is('select') ? who.objective() :
+                  who.val() ? who.val() :
+                  who.is('select') ? who.val() : who.text();
 
-    if (key) result[key] = (result[key] && !jQuery.isArray(result[key])) ? Array(result[key], value) :
-                           jQuery.isArray(result[key]) ? append(result[key], value) : value;
-  });
-  
-  return result;
-};
-
+      if (key) result[key] = (result[key] && !$.isArray(result[key])) ? Array(result[key], value) :
+                             $.isArray(result[key]) ? append(result[key], value) : value;
+    });
+    
+    return result;
+  };
+})(jQuery);
