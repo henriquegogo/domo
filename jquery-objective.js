ff0f2cc3c3dfe8eafeaadc0d5a9e61f62b1465c3
Henrique Gogó <henriquegogo@gmail.com>
2011-11-15 01:50:08 -0300

    Selectors OK. We have all elements with object format

--- jquery-objective.js
+++ jquery-objective.js
@@ -4,16 +4,18 @@
     var result = {};
     
     $(this).children().each(function() {
-      var who = $(this).attr('name') ? $(this) : $(this).children('[name]');
+      var who = $(this).attr('name') ? $(this) : $(this).find('> [name], > :not([name]) [name]');
       var key = who.attr('name');
 
-      var value = who.children('[name]').size() > 0 ? who.objective() :
+      var value = who.find('> [name], > :not([name]) [name]').size() > 0 ? who.objective() :
                   who.val() ? who.val() : who.text();
       
       value = who.is('select') ? who.val() :
               who.is('[type=checkbox]') ? $("[name="+ $(this).attr('name') +"]:checked")[0] ? true : false :
               who.is('[type=radio]') ? $("[name="+ $(this).attr('name') +"]:checked").val() :
               value;
+
+      console.log(result[key]);
 
       if (key) result[key] = (result[key] && !$.isArray(result[key]) && !who.is('[type=radio]')) ? Array(result[key], value) :
                              $.isArray(result[key]) ? append(result[key], value) : value;
