9437d12cbde4e79c73cb75c8401360252d59c341
Henrique Gogó <henriquegogo@gmail.com>
2011-11-18 00:20:50 -0300

    refactoring

--- jquery-domo.js
+++ jquery-domo.js
@@ -7,8 +7,15 @@
       $('[type=checkbox], [type=radio]', this).removeAttr('checked');
       $('option', this).removeAttr('selected');
 
-      var stringDom = function(who) {
-        return $("<div />").html(who).html();
+      var stringDom = function(who) { return $("<div />").html(who).html() };
+
+      var applyValues = function(who, object, prop) {
+        $.isPlainObject(object[prop]) ? who.domo(object[prop]) :
+        $.isArray(object[prop]) && !who.is('[multiple], [type=radio]') ? doArray(who, object[prop]) :
+        who.is('select') ? who.find("option[value='" + object[prop] + "'], option:contains(" + object[prop] + ")").attr('selected', 'true') :
+        who.is('[type=checkbox]') ? who.attr('checked', object[prop]) :
+        who.is('[type=radio]') ? who.filter("[value=" + object[prop] + "]").attr("checked", true) :
+        who.is('input') ? who.attr('value', object[prop]) : who.html(object[prop]);
       }
 
       var doArray = function(who, arr) {
@@ -16,33 +23,17 @@
 
         for (var i = 0; i < arr.length; i++) {
           var clone = who.eq(0).clone();
-
-          $.isPlainObject(arr[i]) ? clone.domo(arr[i]) :
-          $.isArray(arr[i]) && !who.is('[multiple], [type=radio]') ? doArray(clone, arr[i]) :
-          clone.is('select') ? clone.find("option[value='" + arr[i] + "'], option:contains(" + arr[i] + ")").attr('selected', 'true') :
-          clone.is('[type=checkbox]') ? clone.attr('checked', arr[i]) :
-          clone.is('[type=radio]') ? clone.filter("[value=" + arr[i] + "]").attr("checked", true) :
-          clone.is('input') ? clone.attr('value', arr[i]) : clone.html(arr[i]);
-        
+          applyValues(clone, arr, i)
           container.append(stringDom(clone));
         }
         
-        if (who.length > 1) {
-          who.replaceWith(who.eq(0));
-        }
-  
+        if (who.length > 1) who.replaceWith(who.eq(0));
         who.replaceWith(container.html());
       }
 
       for (var prop in object) {
         var who = $('> [name=' + prop + '], > :not([name]) [name=' + prop + ']', this);
-
-        $.isPlainObject(object[prop]) ? who.domo(object[prop]) :
-        $.isArray(object[prop]) && !who.is('[multiple], [type=radio]') ? doArray(who, object[prop]) :
-        who.is('select') ? who.find("option[value='" + object[prop] + "'], option:contains(" + object[prop] + ")").attr('selected', 'true') :
-        who.is('[type=checkbox]') ? who.attr('checked', object[prop]) :
-        who.is('[type=radio]') ? who.filter("[value=" + object[prop] + "]").attr("checked", true) :
-        who.is('input') ? who.attr('value', object[prop]) : who.html(object[prop]);
+        applyValues(who, object, prop)
       }
 
     } else {
