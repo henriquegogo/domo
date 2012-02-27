f9ab3ab1216b4eaca9041c447a93bee0516a19f8
Henrique Gogó <henriquegogo@gmail.com>
2012-02-27 03:03:10 -0300

    closure

--- domo.native.js
+++ domo.native.js
@@ -1,4 +1,4 @@
-//(function () {
+(function () {
   // Some helpers
   var isArray = function(array) {
     return (typeof array == 'object' && array.length != undefined);
@@ -66,6 +66,5 @@
     
     console.log( domo.body );
     console.log( JSON.stringify(domo.body) );
-    //var object = {"fotos":["fotos\/5\/gogs.jpg"],"id":"5","codigo":"1","tipo":"Casa","endereco":"Av. Her\u00f3is do Acre 801\/101A","estado":"CE","cidade":"Fortaleza","bairro":"Passar\u00e9","finalidade":"Venda","pontoReferencia":"","cliente":{"imoveis":[{"imoveis":null,"id":"5","codigo":"1","tipo":"Casa","endereco":"Av. Her\u00f3is do Acre 801\/101A","estado":"CE","cidade":"Fortaleza","bairro":"Passar\u00e9","finalidade":"Venda","pontoReferencia":"","cliente_id":"1","cliente_nome":"Henrique Gog\u00f3","areaPrivativa":"","areaPrivativaMedida":"","areaTerreno":"","areaTerrenoMedida":"","dormitorios":"","suites":"","banheiros":"","pavimentos":"","condominioNome":"","anoConstrucao":"","condominioValor":"","garagem":"N\u00e3o","vagas":"","posicao":"","mobiliado":"N\u00e3o","descricao":"","created":"2012-02-21 02:08:00","updated":"2012-02-21 02:08:07","foto":null}],"id":"1","nome":"Henrique Gog\u00f3","email":"henriquegogo@hotmail.com","endereco":"Av. Her\u00f3is do Acre 801\/101A","estado":"CE","cidade":"Fortaleza","bairro":"Passar\u00e9","telefone":"85 8747-2023","created":"2012-02-20 01:25:50","updated":"2012-02-20 01:25:50"},"cliente_nome":"Henrique Gog\u00f3","areaPrivativa":"","areaPrivativaMedida":"","areaTerreno":"","areaTerrenoMedida":"","dormitorios":"","suites":"","banheiros":"","pavimentos":"","condominioNome":"","anoConstrucao":"","condominioValor":"","garagem":"N\u00e3o","vagas":"","posicao":"","mobiliado":"N\u00e3o","descricao":"","created":"2012-02-21 02:08:00","updated":"2012-02-21 02:08:07","foto":null};
   };
-//})();
+})();
