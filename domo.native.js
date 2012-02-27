50d9463424a4550811e0869f464e2e4863d0ce1d
Henrique Gogó <henriquegogo@gmail.com>
2012-02-27 01:19:21 -0300

    Cleaning code and working on cross compatible

--- domo.native.js
+++ domo.native.js
@@ -13,14 +13,16 @@
       if (key) {
         key = key.replace(/\[]$/, "");
 
-        var value = tag.children.length ?
-                    dom2obj.call(tag) : tag.value || tag.innerText;
+        var value = tag.children.length && tag.querySelector("[name]") ?
+                    dom2obj.call(tag) : tag.value || tag.textContent;
 
-        value = (tag.tagName == 'SELECT') ? tag.value :
-                (tag.getAttribute('type') == 'checkbox') ? tag.checked :
-                (tag.getAttribute('type') == 'radio' && tag.checked) ? tag.value :
-                (tag.getAttribute('type') == 'radio' && !tag.checked) ? result[key] :
+        value = (tag.type == 'checkbox') ? tag.checked :
+                (tag.type == 'radio' && !tag.checked) ? result[key] :
                 value;
+
+        // value = (result[key] && !$.isArray(result[key]) && !$(this).is('[type=radio]') || $(this).attr("name").match(/[]$/) ) ?
+        // Array(result[key], value) : $.isArray(result[key]) ?
+        // append(result[key], value) : value;
 
         result[key] = value;
       
@@ -37,8 +39,11 @@
     if (typeof object == 'object')
       obj2dom(object);
     
-    window.dom = dom2obj.call(document.body);
-
+    window.domo = window.domo || {};
+    window.domo.body = window.domo.body || dom2obj.call(document.body);
+    
+    console.log( domo.body );
+    console.log( JSON.stringify(domo.body) );
     //var object = {"fotos":["fotos\/5\/gogs.jpg"],"id":"5","codigo":"1","tipo":"Casa","endereco":"Av. Her\u00f3is do Acre 801\/101A","estado":"CE","cidade":"Fortaleza","bairro":"Passar\u00e9","finalidade":"Venda","pontoReferencia":"","cliente":{"imoveis":[{"imoveis":null,"id":"5","codigo":"1","tipo":"Casa","endereco":"Av. Her\u00f3is do Acre 801\/101A","estado":"CE","cidade":"Fortaleza","bairro":"Passar\u00e9","finalidade":"Venda","pontoReferencia":"","cliente_id":"1","cliente_nome":"Henrique Gog\u00f3","areaPrivativa":"","areaPrivativaMedida":"","areaTerreno":"","areaTerrenoMedida":"","dormitorios":"","suites":"","banheiros":"","pavimentos":"","condominioNome":"","anoConstrucao":"","condominioValor":"","garagem":"N\u00e3o","vagas":"","posicao":"","mobiliado":"N\u00e3o","descricao":"","created":"2012-02-21 02:08:00","updated":"2012-02-21 02:08:07","foto":null}],"id":"1","nome":"Henrique Gog\u00f3","email":"henriquegogo@hotmail.com","endereco":"Av. Her\u00f3is do Acre 801\/101A","estado":"CE","cidade":"Fortaleza","bairro":"Passar\u00e9","telefone":"85 8747-2023","created":"2012-02-20 01:25:50","updated":"2012-02-20 01:25:50"},"cliente_nome":"Henrique Gog\u00f3","areaPrivativa":"","areaPrivativaMedida":"","areaTerreno":"","areaTerrenoMedida":"","dormitorios":"","suites":"","banheiros":"","pavimentos":"","condominioNome":"","anoConstrucao":"","condominioValor":"","garagem":"N\u00e3o","vagas":"","posicao":"","mobiliado":"N\u00e3o","descricao":"","created":"2012-02-21 02:08:00","updated":"2012-02-21 02:08:07","foto":null};
   };
 //})();
