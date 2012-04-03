b238882266bbfd9e7c692c170bf0acd12e351976
Henrique Gogó <henriquegogo@gmail.com>
2012-04-02 23:54:05 -0300

    Improving Object To Dom tests

--- tests/spec.js
+++ tests/spec.js
@@ -40,6 +40,57 @@ describe("Domo", function() {
       expect(html).toMatch('One item list');
       expect(html).toMatch('Other item in one item list');
     });
+
+    it("can change nested proprieties in array tags", function() {
+      var listElement = document.getElementById("test-children-tags").cloneNode(true);
+      object.toDom(listElement);
+      var html = listElement.innerHTML;
+      var items = listElement.querySelectorAll("[name='parents[]']")
+      
+      expect(items.length).toEqual(1);
+      expect(html).toMatch('Henrique');
+      expect(html).toMatch('Mariah');
+      expect(html).toMatch('Mary');
+      expect(html).toMatch('Smith');
+      expect(html).toMatch('James');
+    });
+
+    it("can set attribute value with variable marks", function() {
+      var listElement = document.getElementById("name").cloneNode(true);
+      object.toDom(listElement);
+      var html = listElement.outerHTML;
+      
+      expect(html).toMatch('/user/David');
+    });
+
+    it("can fill form fields", function() {
+      var listElement = document.getElementById("test-form").cloneNode(true);
+      object.toDom(listElement);
+      var html = listElement.innerHTML;
+      var city = listElement.querySelector("[name=city]");
+      
+      expect(html).toMatch('Diane');
+      expect(html).toMatch('What do you want?');
+      expect(html).toMatch('"Female" checked="true"');
+      expect(html).toMatch('"human" checked="true"');
+      expect(html).toMatch('"emails" checked="true"');
+      expect(city.value).toEqual('FOR');
+    });
+
+    it("can fill form fields with string representation of object.property", function() {
+      var listElement = document.getElementById("test-props").cloneNode(true);
+      object.toDom(listElement);
+      var client_id = listElement.querySelector("[name='client.id']");
+      var client_name = listElement.querySelector("[name='client.name']");
+      var product_identify = listElement.querySelector("[name='product[Identify]']");
+      var product_firstName = listElement.querySelector("[name='product[Identify]']");
+      var html = listElement.outerHTML;
+      
+      // expect(client_id.value).toEqual('1');
+      // expect(client_name.value).toEqual('Gilbert');
+      // expect(product_identify.value).toEqual('33');
+      // expect(product_firstName.value).toEqual('iPod');
+    });
   });
 
   describe("DOM to Object", function() {
