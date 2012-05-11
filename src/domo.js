55d179e043013086c2567bc30014207f06b88ed1
Henrique Gogó <henriquegogo@gmail.com>
2012-05-11 11:27:04 -0300

    Changing methods to jQuery methods. Not working yet. Dont applying values

--- src/domo.js
+++ src/domo.js
@@ -17,29 +17,29 @@
 
     // Object to DOM
     window.toDom = function(object, container) {
-        var container = container || document.body;
-        var el = (container.name) ? [container] : container.children;
+        var container = $(container) || $(document);
+        var el = (container.attr('name')) ? container : container.children();
         
         var uncheck = function(tag) {
-            if (tag.type == 'checkbox' || tag.type == 'radio') {
-                tag.removeAttribute('checked');
-                tag.checked = false;
-            } else if (tag.querySelector("[selected]"))
-                tag.querySelector("[selected]").removeAttribute('selected');
+            if (tag.attr('type') == 'checkbox' || tag.attr('type') == 'radio') {
+                tag.removeAttr('checked');
+                tag[0].checked = false;
+            } else if (tag.find("[selected]"))
+                tag.find("[selected]").removeAttr('selected');
         };
 
         var removeSiblings = function(tag, name) {
-            var siblings = tag.parentElement.querySelectorAll("[name='"+name+"']");
-            for (var i = 1; i < siblings.length; i++) {
-                try { tag.parentElement.removeChild(siblings[i]); }
+            var siblings = tag.parent().find("[name='"+name+"']");
+            siblings.each(function() {
+                try { tag.parent()[0].removeChild(this); }
                 catch(err) { }
-            }
+            });
         };
 
         var setSelectTagValue = function(tag, value) {
-            tag.value = value;
+            tag.val(value);
             // This code below is just because iE needs
-            var options = tag.children;
+            var options = tag.children();
             for (var i in options) {
                 if (options[i].value == value)
                     options[i].selected = true;
@@ -49,56 +49,56 @@
         var applyValues = function(tag, object, key) {
             key = "" + key;
             key.match(/\./) || key.match(/\[[a-zA-Z].*]/) ? splitKey(tag, object, key) :
-            isArray(object[key]) ? doArray(tag, object[key], name) :
-            isObject(object[key]) ? toDom(object[key], tag) :
-            tag.tagName == 'IMG' ? tag.setAttribute('src', object[key]) :
-            tag.tagName == 'SELECT' ? setSelectTagValue(tag, object[key]) :
-            tag.type == 'checkbox' && object[key] ? tag.checked = true :
-            tag.type == 'radio' && tag.value == object[key] ? tag.checked = true :
-            tag.tagName == 'BUTTON' || tag.type == 'file' || tag.type == 'radio' || tag.type == 'checkbox' ? false :
-            tag.tagName == 'INPUT' ? tag.value = object[key] :
-            tag.innerHTML = object[key] || "";
+            $.isArray(object[key]) ? doArray(tag, object[key], name) :
+            $.isObject(object[key]) ? toDom(object[key], tag) :
+            tag.prop('tagName') == 'IMG' ? tag.attr('src', object[key]) :
+            tag.prop('tagName') == 'SELECT' ? setSelectTagValue(tag, object[key]) :
+            tag.attr('type') == 'checkbox' && object[key] ? tag[0].checked = true :
+            tag.attr('type') == 'radio' && tag.value == object[key] ? tag[0].checked = true :
+            tag.prop('tagName') == 'BUTTON' || tag.attr('type') == 'file' || tag.attr('type') == 'radio' || tag.attr('type') == 'checkbox' ? false :
+            tag.prop('tagName') == 'INPUT' ? tag.val() = object[key] :
+            tag.html(object[key] || "");
         };
 
         var applyAttributesVariables = function(tag, object) {
-            var attributes = tag.attributes;
+            var attributes = tag[0].attributes;
 
             for (var i = 0; i < attributes.length; i++)
                 if ( attributes[i].value.match(/{\w*}/g) ) {
                     var attrName = attributes[i].name;
                     var attrValue = attributes[i].value.replace(/{(\w*)}/gi, function(m, key) { return object[key]; });
-                    setTimeout(function() { tag.setAttribute(attrName, attrValue); }, 0);
+                    setTimeout(function() { tag.attr(attrName, attrValue); }, 0);
                 }
         };
 
         var splitKey = function(tag, object, key) {
             var keys = key.match(/\./) ? key.split(".") : key.split("[");
             keys[1] = keys[1].replace("]", "");
-            if ( isObject(object[keys[0]]) )
+            if ( $.isObject(object[keys[0]]) )
                 applyValues(tag, object[keys[0]], keys[1]);
         };
 
         var doArray = function(tag, arr, name) {
-            if (tag === tag.parentElement.querySelector("[name='"+name+"']")) {
+            if (tag === tag.parent().find("[name='"+name+"']")) {
                 removeSiblings(tag, name);
                 applyValues(tag, arr, 0);
 
                 for (var i = 1; i < arr.length; i++) {
-                    var clone = tag.cloneNode(true);
+                    var clone = tag.clone();
                     applyValues(clone, arr, i);
-                    tag.parentElement.appendChild(clone);
+                    tag.parent().append(clone);
                 }
             }
         };
 
-        for (var i = 0; i < el.length; i++) {
-            var tag = el[i];
-            var key = name = tag.getAttribute("name");
+        el.each(function(){
+            var tag = $(this);
+            var key = name = tag.attr("name");
 
             if (key) {
                 key = key.replace(/\[\d*]$/, "");
 
-                if (tag.children.length && tag.querySelector("[name]") && isObject(object[key]) ) {
+                if (tag.children().length && tag.find("[name]").length && $.isPlainObject(object[key]) ) {
                     toDom(object[key], tag);
 
                 } else {
@@ -111,7 +111,7 @@
             }
 
             applyAttributesVariables(tag, object);
-        }
+        });
 
         return container;
     };
